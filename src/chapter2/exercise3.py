2c95bf092cac83e88072a94567ed8a1b16b3ebcc
group13bse1 <2020bse165@std.must.ac.ug>
2021-03-23 18:26:29 +0300

    assignment from chapter 2

---
+++ src/chapter2/exercise3.py
@@ -0,0 +1,4 @@
+hours=int (input('Enter Hours \n'))
+rate=float(input('Enter Rate\n'))
+pay=rate*hours
+print('Pay: ', pay)
---
+++ src/chapter2/exercise5.py
@@ -0,0 +1,3 @@
+temp_celcius=float(input('Enter the temperature in celcius\n'))
+temp_fahrenheit=(temp_celcius*9/5)+32
+print( "\n {} celcius is {} in Fahrenheit".format(temp_celcius,temp_fahrenheit ))
