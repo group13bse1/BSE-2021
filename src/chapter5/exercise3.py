a609d0e4247ca99f24493f472e891de6c5f8c513
group13bse1 <2020bse165@std.must.ac.ug>
2021-04-23 02:00:30 +0300

    attempt 2 on exercise 3, note. program not completed.

--- src/chapter5/exercise3.py
+++ src/chapter5/exercise3.py
@@ -7,6 +7,14 @@ quater = 25 # value is 0.25
 Ones = 0 # value is 1
 fives = 0 # value is 5
 
+#starting point (first display)
+print("\n Welcome to the vending machine change maker program")
+#function to turn balance into required format.
+def dollars_and_cents(money_passed):
+    dol_av = int(money_passed /100)
+    cent_av = money_passed % 100
+    print(f"Payment due is: {dol_av} dollars and {cent_av} cents\n")
+
 #create endless loop for entire program
 while True:
     print(
@@ -18,8 +26,9 @@ while True:
         f'\nOnes   {Ones}'
         f'\nFives  {fives}'
     )
+    #this is the price of the product some one is buying from the v.machine
     input_data = input("Enter the price in form of XX.XX or q to quit..: ")
-    if input_data == "q":
+    if input_data.lower() == "q":
         print("Quited....")
         break
     elif input_data != "q":
@@ -27,13 +36,73 @@ while True:
 
             new_price = float(input_data)
             #turn it into integer make it easy to use
-            bal = int(new_price*100)
+            bal = new_price*100
+            #loop; captures customers deposits
+            while True:
+                if (bal > 0 and (bal % 5 == 0)):
+                    choice = input(
+                        "\nMenu for deposits:\n"
+                        "'n' - deposit a nickel\n"
+                        "'d' - deposit a dime\n"
+                        "'q' - deposit a quarter\n"
+                        "'o' - deposit a one dollar bill\n"
+                        "'f' - deposit a five dollar bill\n"
+                                   )
+                    # if choice != "d" or "n" or "q" or "o" or "f":
 
-            #will add the function updating stock for
+                    if (choice == "d"):
+                        bal -= 25
+                        dimes += 1
+                    elif choice == "n":
+                        bal -= 5
+                        nickel += 1
+                    elif choice == "q":
+                        bal -= 10
+                        quater += 1
+                    elif choice == "o":
+                        bal -= 100
+                        Ones += 1
+                    elif choice == "f":
+                        bal -= 500
+                        fives += 1
+                    elif choice == "c":
+                        break
+                    else:
+                        print(f"{choice} is not valid selection")
+                    dollars_and_cents(bal)
+                    print("indicate your deposit")
+                elif new_price < 0:
+                    print("illegal entry...")
+                elif bal < 0:
+                    #at this point, the customer has credit in the machine.
+                    #note. the loop should exit the moment this happens
+                    #note2. calculate the change based on the available stock
+                    change = float(bal * -1) #make it positive for easy calculation
+
+                    balance_in_fives = change // 500
+                    change = change % 500
+                    if balance_in_fives > fives:
+                        change = change + (balance_in_fives - fives)
+                        balance_in_fives = fives
+                        fives = 0
+
+                    balance_in_ones = change // 100
+                    change = change % 100
+                    if balance_in_ones > Ones:
+                        change = change + (balance_in_ones - Ones)
+                        balance_in_ones = Ones
+                        Ones = 0
+                        # this will be repetitive, maybe a function??
+
+
+                else:
+                    print("Enter correct price foramt")
+                    break
 
         except Exception as e:
             print(e)
             # print("Plizz enter l")
+            #remeber to remove this when code is good and ready
             des = input('Pres "N" to enter a new value or "Q" to quit: ')
             if des == "Q":
                 break
