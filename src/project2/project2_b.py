4252564c59a85ddf19e88ee760084155034cdd52
group13bse1 <2020bse165@std.must.ac.ug>
2021-05-16 17:19:22 +0300

    project2_b second commit

--- src/project2/project2_b.py
+++ src/project2/project2_b.py
@@ -9,12 +9,37 @@ def open_file():
             fileHandle = open(fileName)
             break
         except:
-            print('File not available. Try again')
+            print('File not available. Try again with correct file name')
     return fileHandle
-def process_file(fileobject):
-    year = input('Please enter the year')
-    income = input('Please enter income in this form:'
-                   '\n\t 1 \t low income'
-                   '\n\t 2 \t lower middle income'
-                   '\n\t 3 \t upper middle income'
-                   '\n\t 4 \t high income\n')
+def process_file(fileObject):
+    yearEnterd = input('Please enter the year\n')
+    while True:
+        incomeSelected = input('Please select income:'
+                               '\n\t 1 \t low income'
+                               '\n\t 2 \t lower middle income'
+                               '\n\t 3 \t upper middle income'
+                               '\n\t 4 \t high income\n')
+        if incomeSelected == '1':
+            incomeIs = 'WB_LI'
+            break
+        elif incomeSelected == '2':
+            incomeIs = 'WB_LMI'
+            break
+        elif incomeSelected == '3':
+            incomeIs = 'WB_UMI'
+            break
+        elif incomeSelected == '4':
+            incomeIs = 'WB_HI'
+            break
+        else:
+            print('You entered false value, try again')
+    counter = 0
+    total = 0
+    country = None
+    for line in fileObject:
+        if (line.find(incomeIs) != -1) and (line.find(yearEnterd) != -1):
+            print(line.strip())
+
+
+selectedFile = open_file()
+process_file(selectedFile)
